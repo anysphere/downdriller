4fae54bd8cb15c1585f8fa67a415fbe80978d4e8
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2022-08-27 12:48:50 -0400

    remove cmdk

--- app/src-tauri/src/main.rs
+++ app/src-tauri/src/main.rs
@@ -1,16 +1,30 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
+#[cfg(target_os = "macos")]
+#[macro_use]
+extern crate objc;
+
+use tauri::{Manager, WindowEvent};
+use window_ext::WindowExt;
+
+mod window_ext;
+
 fn main() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .setup(|app| {
+            let win = app.get_window("main").unwrap();
+            win.set_transparent_titlebar(true);
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
 
 #[tauri::command]
 fn greet(name: &str) -> String {
-   format!("Hello, {}!", name)
+    format!("Hello, {}!", name)
 }
--- app/src/App.tsx
+++ app/src/App.tsx
@@ -13,17 +13,24 @@ import {
 import { Editor } from "./Editor";
 import { EditorState } from "lexical";
 import { useRef } from "react";
+import CommandPalette, { useHandleOpenCommandPalette } from "./cmdk";
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
+}
+
+enum CommandPalettePage {
+  Root = "ROOT",
 }
 
 function App() {
   const editorStateRef = useRef<EditorState>(null);
 
   return (
-    <div className="px-2 py-2">
-      <Editor className="w-full" editorStateRef={editorStateRef} />
+    <div className="absolute top-0 left-0 right-0 bottom-0 bg-red-500">
+      <div className="px-2 py-2 bg-green-100">
+        <Editor className="w-full" editorStateRef={editorStateRef} />
+      </div>
     </div>
   );
 }
